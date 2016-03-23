fe1521ac737c3b6bacd07521c81a2402c4348ea6
Louis Popi <theh2o64@gmail.com>
2016-03-23 21:31:00 +0100

    keyhandler: Code cleanup and add circle gesture

--- src/org/paradox/keyhandler/KeyHandler.java
+++ src/org/paradox/keyhandler/KeyHandler.java
@@ -35,60 +35,82 @@ import com.android.internal.os.DeviceKeyHandler;
 import com.android.internal.util.ArrayUtils;
 
 public class KeyHandler implements DeviceKeyHandler {
+
 	private static final String TAG = KeyHandler.class.getSimpleName();
 	private static final boolean DEBUG = true;
 	private static final int GESTURE_REQUEST = 1;
-    private static final int GESTURE_WAKELOCK_DURATION = 3000;
+	private static final int GESTURE_WAKELOCK_DURATION = 3000;
 	
 	// Supported scancodes
-	private static final int GESTURE_V_SCANCODE = 252;
+	private static final int GESTURE_CIRCLE_SCANCODE = 62;
+	private static final int GESTURE_V_SCANCODE = 63;
 	private static final int MODE_TOTAL_SILENCE = 600;
 	private static final int MODE_ALARMS_ONLY = 601;
 	private static final int MODE_PRIORITY_ONLY = 602;
 	private static final int MODE_NONE = 603;
 	
 	private static final int[] sSupportedGestures = new int[]{
-        GESTURE_V_SCANCODE,
+        	GESTURE_CIRCLE_SCANCODE,
+        	GESTURE_V_SCANCODE,
 		MODE_TOTAL_SILENCE,
 		MODE_ALARMS_ONLY,
 		MODE_PRIORITY_ONLY,
 		MODE_NONE,
-    };
+    	};
 	
 	private static final int[] sHandledGestures = new int[]{
-        GESTURE_V_SCANCODE,
-    };
+		GESTURE_V_SCANCODE,
+	};
 	
 	private static final SparseIntArray sSupportedSliderModes = new SparseIntArray();
-    static {
-        sSupportedSliderModes.put(MODE_TOTAL_SILENCE, Settings.Global.ZEN_MODE_NO_INTERRUPTIONS);
-        sSupportedSliderModes.put(MODE_ALARMS_ONLY, Settings.Global.ZEN_MODE_ALARMS);
-        sSupportedSliderModes.put(MODE_PRIORITY_ONLY, Settings.Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS);
-        sSupportedSliderModes.put(MODE_NONE, Settings.Global.ZEN_MODE_OFF);
-    }
+	static {
+        	sSupportedSliderModes.put(MODE_TOTAL_SILENCE, Settings.Global.ZEN_MODE_NO_INTERRUPTIONS);
+		sSupportedSliderModes.put(MODE_ALARMS_ONLY, Settings.Global.ZEN_MODE_ALARMS);
+		sSupportedSliderModes.put(MODE_PRIORITY_ONLY, Settings.Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS);
+		sSupportedSliderModes.put(MODE_NONE, Settings.Global.ZEN_MODE_OFF);
+   	};
 	
 	private final Context mContext;
-    private final PowerManager mPowerManager;
+	private final PowerManager mPowerManager;
 	private final NotificationManager mNotificationManager;
-    private EventHandler mEventHandler;
-    private WakeLock mGestureWakeLock;
-    private Handler mHandler = new Handler();
+	private EventHandler mEventHandler;
+	private WakeLock mGestureWakeLock;
+	private Handler mHandler = new Handler();
 	private Vibrator mVibrator;
 
 	public KeyHandler(Context context) {
-        mContext = context;
-        mEventHandler = new EventHandler();
-        mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-		mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        mGestureWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "GestureWakeLock");
+		mContext = context;
+		mEventHandler = new EventHandler();
+		mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+			mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		mGestureWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "GestureWakeLock");
 		mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-    }
+    	}
 	
 	private class EventHandler extends Handler {
         @Override
         public void handleMessage(Message msg) {
             KeyEvent event = (KeyEvent) msg.obj;
             switch(event.getScanCode()) {
+           			 case GESTURE_CIRCLE_SCANCODE:
+           			 	if (DEBUG) Log.i(TAG, "GESTURE_CIRCLE_SCANCODE");
+           			 	ensureKeyguardManager();
+           			 	String action = null;
+           			 	mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
+           			 	if (mKeyguardManager.isKeyguardSecure() && mKeyguardManager.isKeyguardLocked()) {
+           			 		action = MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA_SECURE;
+           			 	} else {
+           			 		try {
+            			 			WindowManagerGlobal.getWindowManagerService().dismissKeyguard();
+           			 	} catch (RemoteException e) {
+           			 		// Ignore
+           			 	}
+           			 		action = MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA;
+           			 	}
+           			 	mPowerManager.wakeUp(SystemClock.uptimeMillis(), "android.policy:GESTURE");
+           			 	Intent intent = new Intent(action, null);
+           			 	startActivitySafely(intent);
+                			break;
 				case GESTURE_V_SCANCODE:
 					if (DEBUG) Log.i(TAG, "GESTURE_V_SCANCODE");
 					mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
